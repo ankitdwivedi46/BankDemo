54cc66141e33eccb45061d660253859d365e2181
Ankit Dwivedi <ankit1.dwivedi@paytm.com>
2022-05-05 01:27:22 +0530

    Withdraw Module

---
+++ MoneyWithdrawlModuel/RunMoneyWithdraw.py
@@ -0,0 +1,24 @@
+from Utility.CommonFunctions import CommonFunctions
+
+class MoneyWithdraw:
+
+    username = ""
+    password = ""
+    pin = ""
+
+    def fetchDetailsFromUser(self):
+
+        cfObj = CommonFunctions()
+
+        self.username = input("Enter User Name to Login: ")
+        choice = input("Want to login using PIN or Password: ")
+        if(choice.upper() == "PIN"):
+            self.pin = input("Enter the PIN").upper()
+            cfObj.validateCustomerPIN(self.pin)
+        elif(choice.upper()=="PASSWORD"):
+            self.password = input("Enter the Password: ")
+            cfObj.validateCustomerPassword(self.password)
+
+
+
+
--- Utility/CommonFunctions.py
+++ Utility/CommonFunctions.py
@@ -13,12 +13,26 @@ class CommonFunctions:
 
         conn.close()
 
-    def validateCustomerCred(self,acc_no):
+    def validateCustomerPIN(self,pin):
 
         dbconn = DBConnections()
         conn = dbconn.connect_to_sql_server()
 
-        assert len(pd.read_sql_query("SELECT * FROM CUSTOMER_CREDENTIAL_DATABANK WHERE CUSTOMER_ACC_NO='{}'".format(acc_no), conn)) == 0
+        assert len(pd.read_sql_query("SELECT * FROM CUSTOMER_CREDENTIAL_DATABANK WHERE CUSTOMER_PIN='{}'".format(pin), conn)) == 0
+
+        conn.close()
+
+    def validateCustomerPassword(self,password):
+
+        dbconn = DBConnections()
+        conn = dbconn.connect_to_sql_server()
+        from cryptography.fernet import Fernet
+        key = Fernet.generate_key()
+
+        cipher_suite = Fernet(key)
+        ciphered_text = cipher_suite.decrypt(bytes(password, "utf-8"))
+
+        assert len(pd.read_sql_query("SELECT * FROM CUSTOMER_CREDENTIAL_DATABANK WHERE CUSTOMER_PASSWORD='{}'".format(password), conn)) == 0
 
         conn.close()
 
